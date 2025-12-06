1bc24b66bbf519c29fe6f582cfa21359af7f8cea
Tarun Koshti <tarunkoshti811@gmail.com>
2025-12-06 13:03:55 +0530

    Change version to v3

--- app/index.js
+++ app/index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 // counter variable to keep track of requests
 let count = 0;
 // Default message
-const message = "Welcome to the Node-Example API - Deployed with ArgoCD-V2!";
+const message = "Welcome to the Node-Example API - Deployed with ArgoCD - V3!";
 
 const app = express();
 app.use(cors());
